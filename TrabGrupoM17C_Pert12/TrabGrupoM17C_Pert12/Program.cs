a1dbd7732320872e9df65888ebc0004946ca20c9
duarte rolim <drutghost@gmail.com>
2025-10-28 17:19:42 +0000

    ddad

--- TrabGrupoM17C_Pert12/TrabGrupoM17C_Pert12/Program.cs
+++ TrabGrupoM17C_Pert12/TrabGrupoM17C_Pert12/Program.cs
@@ -10,6 +10,29 @@ namespace TrabGrupoM17C_Pert12
     {
         static void Main(string[] args)
         {
+            int[] numeros = new int[10] { 5, 11, 1, 23, 5, 2, 9, 1, 8, 67 };
+
+            int indiceMaiorValor = 0;
+            int maiorValor = numeros[0];
+
+
+            for (int i = 1; i < numeros.Length; i++)
+            {
+
+                if (numeros[i] > maiorValor)
+                {
+                    maiorValor = numeros[i];
+                    indiceMaiorValor = i;
+
+                }
+            }
+
+            
+            Console.WriteLine($"O maior valor é {maiorValor} e encontra-se no índice {indiceMaiorValor}.");
+            Console.ReadLine();
         }
     }
+
 }
+
+    
